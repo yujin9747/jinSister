6a28bb4d1238f957ac24a8682523a7382ef64207
jang-yujin <jyj9747@naver.com>
2024-06-29 22:54:12 +0900

    react-snap 적용

--- src/main/frontend/src/index.tsx
+++ src/main/frontend/src/index.tsx
@@ -3,17 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
+const container = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(container);
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (container.hasChildNodes()) {
+    ReactDOM.hydrateRoot(
+        container,
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+} else {
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
